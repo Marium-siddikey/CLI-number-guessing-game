2b31432262da887e017bdad8ab1d7ba271bdf93c
Marium-siddikey <mariumsiddiqui135@gmail.com>
2024-05-19 22:04:57 +0500

    Adding a number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\twelcome to mariumsiddikey - CLI Number guessing game\n");
+
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type:"number",
+        message: "enter your guess number(number limit from 1-3):",
+    },
+]);
+
+if (answer.userGuessedNumber === randomNumber){
+console.log("congratulation you guess the correct number.");
+}
+else{
+    console.log("sorry, your guess was wrong");
+}
